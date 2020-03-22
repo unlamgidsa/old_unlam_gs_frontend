a5a498bb5d413b36e986fdcc1d9a88aafe94f232
NewtonGauss <germanmerkel9@gmail.com>
2020-03-22 20:51:22 -0300

    Deleted simpleVuePlugin from index Implemented (front) login from indicator TODO: login form TODO: log out

--- src/plugins/login/components/globalLoginIndicator.vue
+++ src/plugins/login/components/globalLoginIndicator.vue
@@ -1,20 +1,31 @@
 <template>
-<div class="c-indicator c-indicator--clickable icon-person s-status-caution">
-    <span class="label c-indicator__label">
-        <button @click="globalLoginEmit">Change user</button>
-    </span>
-</div>
+	<div class="c-indicator c-indicator--clickable icon-person s-status-caution">
+		<span class="label c-indicator__label">
+			<button @click="globalLoginEmit">{{ username }}</button>
+		</span>
+	</div>
 </template>
 
 <script>
+import axios from 'axios';
+import APIKey from '../api.js';
 export default {
-    inject: ['openmct'],
-    methods: {
-        globalLoginEmit() {
-            this.openmct.objectViews.emit('login');
-            console.log("globalLoginEmit");
-            
-        }
-    }
+	inject: ['openmct'],
+	methods: {
+		globalLoginEmit() {
+			axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + APIKey(), {
+				email: 'test4@test.com',
+				password: '123456',
+				returnSecureToken: true
+			}).then(response => {
+				this.username = response.data.email;
+			});
+		}
+	},
+	data() {
+		return {
+			username: 'Change user'
+		};
+	}
 }
 </script>
