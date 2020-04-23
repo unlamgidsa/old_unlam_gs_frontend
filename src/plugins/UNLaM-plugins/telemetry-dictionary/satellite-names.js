f5b869413f5dcdff78a1258a99ad52ee02578b29
NewtonGauss <germanmerkel9@gmail.com>
2020-04-23 12:54:41 -0300

    Some code moved inside SatelliteNames for a test. Commented out former code just in case.

--- src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
+++ src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
@@ -1,9 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default function SatelliteNames(url) {
-  return axios.get(url).then(response => {
-    return response.data.map(satellite => {
-      return { name: satellite.code, key: 'satellite' };
-    });
-  });
+export default function SatelliteNames(
+	urlBase,
+	listParam,
+	TlmyParam,
+	openmct
+) {
+	axios
+		.get(urlBase + listParam)
+		.then(response => {
+			return response.data.map(satellite => {
+				return { name: satellite.code, key: "satellite" };
+			});
+		})
+		.then(satellite => {
+			for (let sat of satellite) {
+				openmct.install(
+					openmct.plugins.TelemetryDictionaryPlugin(
+						sat.name,
+						sat.key,
+						urlBase + TlmyParam + sat.name
+					)
+				);
+			}
+		});
 }
