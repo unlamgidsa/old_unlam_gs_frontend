101b0e07e4264884bf2d02384bc84ad799ee1335
NewtonGauss <germanmerkel9@gmail.com>
2020-04-28 15:53:54 -0300

    rearranged satellites names call

--- src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
+++ src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
@@ -1,27 +1,9 @@
 import axios from "axios";
 
-export default function SatelliteNames(
-	urlBase,
-	listParam,
-	TlmyParam,
-	openmct
-) {
-	axios
-		.get(urlBase + listParam)
-		.then(response => {
-			return response.data.map(satellite => {
-				return { name: satellite.code, key: "satellite" };
-			});
-		})
-		.then(satellite => {
-			for (let sat of satellite) {
-				openmct.install(
-					openmct.plugins.TelemetryDictionaryPlugin(
-						sat.name,
-						sat.key,
-						urlBase + TlmyParam + sat.name
-					)
-				);
-			}
+export default async function SatelliteNames(url) {
+	return axios.get(url).then(response => {
+		return response.data.map(satellite => {
+			return { name: satellite.code, key: "satellite" };
 		});
+	});
 }
