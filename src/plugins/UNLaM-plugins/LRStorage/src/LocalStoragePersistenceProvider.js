ac8320a948058de9537086ca763c23bc0484b934
NewtonGauss <germanmerkel9@gmail.com>
2020-05-10 18:37:52 -0300

    clean

--- src/plugins/UNLaM-plugins/LRStorage/src/LocalStoragePersistenceProvider.js
+++ src/plugins/UNLaM-plugins/LRStorage/src/LocalStoragePersistenceProvider.js
@@ -1,105 +1,107 @@
-define(
-	["../../http-server/service.js"],
-	function (http) {
+define(["../../http-server/service.js"], function(http) {
+	/**
+	 * The LocalStoragePersistenceProvider reads and writes JSON documents
+	 * (more specifically, domain object models) to/from the browser's
+	 * local storage.
+	 * The RemotePersistance added the functionality for user storage
+	 * @memberof platform/persistence/local
+	 * @constructor
+	 * @implements {PersistenceService}
+	 * @param q Angular's $q, for promises
+	 * @param $http 
+	 * @param {string} space the name of the persistence space being served
+	 * @param {string} url from the remote storage
+	 */
+	function LocalStoragePersistenceProvider($window, $q, $http, space, url) {
+		this.$q = $q;
+		this.$http = $http;
+		this.space = space;
+		this.url = url;
+		this.spaces = space ? [space] : [];
+		this.localStorage = $window.localStorage;
+	}
 
-		/**
-		 * The LocalStoragePersistenceProvider reads and writes JSON documents
-		 * (more specifically, domain object models) to/from the browser's
-		 * local storage.
-		 * @memberof platform/persistence/local
-		 * @constructor
-		 * @implements {PersistenceService}
-		 * @param q Angular's $q, for promises
-		 * @param $interval Angular's $interval service
-		 * @param {string} space the name of the persistence space being served
-		 */
-		function LocalStoragePersistenceProvider($window, $q, $http, space, url) {
-			this.$q = $q;
-			this.$http = $http;
-			this.space = space;
-			this.url = url;
-			this.spaces = space ? [space] : [];
-			this.localStorage = $window.localStorage;
-		}
+	/**
+	 * Set a value in local storage.
+	 * Send my items to backend, and server decides storing.
+	 * @private
+	 */
+	LocalStoragePersistenceProvider.prototype.setValue = function(key, value) {
+		this.localStorage[key] = JSON.stringify(value);
+		http.httpPut(this.url, value);
+	};
 
-		/**
-		 * Set a value in local storage.
-		 * @private
-		 */
-		LocalStoragePersistenceProvider.prototype.setValue = function (key, value) {
-			this.localStorage[key] = JSON.stringify(value);
-			//this.$http({
-			//	method: "PUT",
-			//	url: this.url,
-			//	data: value
-			//});
-			//http.httpPut(this.url, value).then(res => console.log(res));
-		};
+	/**
+	 * Get a value from local storage.
+	 * @private
+	 */
+	LocalStoragePersistenceProvider.prototype.getValue = function(key) {
+		return this.localStorage[key] ? JSON.parse(this.localStorage[key]) : {};
+	};
 
-		/**
-		 * Get a value from local storage.
-		 * @private
-		 */
-		LocalStoragePersistenceProvider.prototype.getValue = function (key) {
-			return this.localStorage[key] ?
-				JSON.parse(this.localStorage[key]) : {};
-		};
+	LocalStoragePersistenceProvider.prototype.listSpaces = function() {
+		return this.$q.when(this.spaces);
+	};
 
-		LocalStoragePersistenceProvider.prototype.listSpaces = function () {
-			return this.$q.when(this.spaces);
-		};
+	LocalStoragePersistenceProvider.prototype.listObjects = function(space) {
+		return this.$q.when(Object.keys(this.getValue(space)));
+	};
 
-		LocalStoragePersistenceProvider.prototype.listObjects = function (space) {
-			return this.$q.when(Object.keys(this.getValue(space)));
-		};
+	LocalStoragePersistenceProvider.prototype.createObject = function(
+		space,
+		key,
+		value
+	) {
+		var spaceObj = this.getValue(space);
+		spaceObj[key] = value;
+		this.setValue(space, spaceObj);
+		return this.$q.when(true);
+	};
 
-		LocalStoragePersistenceProvider.prototype.createObject = function (space, key, value) {
-			var spaceObj = this.getValue(space);
-			spaceObj[key] = value;
-			this.setValue(space, spaceObj);
-			return this.$q.when(true);
-		};
-
-		LocalStoragePersistenceProvider.prototype.readObject = function (space, key) {
-			return this.$http({ // cambiar por el httpGet de http para el usuario
-				method: "GET",
-				url: this.url
-			}).then(response => {
+	LocalStoragePersistenceProvider.prototype.readObject = function(space, key) {
+		return http.httpGet(this.url).then(
+			response => {
 				//this.localStorage[space] = JSON.stringify(response.data);
 				//return response.data[key];
 				let resData = response.data,
 					localData = this.getValue(space),
-					spaceObj = {...localData, ...resData};
-				spaceObj.mine.composition = [...localData.mine.composition, ...resData.mine.composition].unique();
+					spaceObj = { ...localData, ...resData };
+				spaceObj.mine.composition = [
+					...localData.mine.composition,
+					...resData.mine.composition
+				].unique();
 				return spaceObj[key];
-			}, error => {
+			},
+			error => {
 				var spaceObj = this.getValue(space);
 				return spaceObj[key];
-			});
-		};
+			}
+		);
+	};
 
-		LocalStoragePersistenceProvider.prototype.deleteObject = function (space, key) {
-			var spaceObj = this.getValue(space);
-			delete spaceObj[key];
-			this.setValue(space, spaceObj);
-			return this.$q.when(true);
-		};
+	LocalStoragePersistenceProvider.prototype.deleteObject = function(
+		space,
+		key
+	) {
+		var spaceObj = this.getValue(space);
+		delete spaceObj[key];
+		this.setValue(space, spaceObj);
+		return this.$q.when(true);
+	};
 
-		LocalStoragePersistenceProvider.prototype.updateObject =
-			LocalStoragePersistenceProvider.prototype.createObject;
+	LocalStoragePersistenceProvider.prototype.updateObject =
+		LocalStoragePersistenceProvider.prototype.createObject;
 
-		Array.prototype.unique = function() {
-    var a = this.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
+	Array.prototype.unique = function() {
+		var a = this.concat();
+		for (var i = 0; i < a.length; ++i) {
+			for (var j = i + 1; j < a.length; ++j) {
+				if (a[i] === a[j]) a.splice(j--, 1);
+			}
+		}
 
-    return a;
-};
+		return a;
+	};
 
-		return LocalStoragePersistenceProvider;
-	}
-);
+	return LocalStoragePersistenceProvider;
+});
