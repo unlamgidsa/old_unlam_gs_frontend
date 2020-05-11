c93ddcfe3a3c2ed7e6b7f03657405949f3045a86
NewtonGauss <germanmerkel9@gmail.com>
2020-05-11 20:01:20 -0300

    Asks for sats w/ user data

--- src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
+++ src/plugins/UNLaM-plugins/telemetry-dictionary/satellite-names.js
@@ -1,9 +1,10 @@
-import axios from "axios";
+import * as http from "../http-server/service.js";
 
 export default async function SatelliteNames(url) {
-	return axios.get(url).then(response => {
+	return http.httpGet(url).then(response => {
 		return response.data.map(satellite => {
 			return { name: satellite.code, key: "satellite" };
 		});
 	});
+	
 }
