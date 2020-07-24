dc559a4c6087a81c86ab86a1005b61eb59f7f92f
NewtonGauss <germanmerkel9@gmail.com>
2020-07-24 18:27:50 -0300

    Muestra la ultima telemetria

--- src/plugins/UNLaM-plugins/historical-telemetry/telemetry.provider.js
+++ src/plugins/UNLaM-plugins/historical-telemetry/telemetry.provider.js
@@ -1,48 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
 
 export class TelemetryProvider {
-  constructor(openmct, urlBase) {
-    this._openmct = openmct;
-    this._urlBase = urlBase;
-  }
+	constructor(openmct, urlBase) {
+		this._openmct = openmct;
+		this._urlBase = urlBase;
+	}
 
-  addProvider() {
-    this._openmct.telemetry.addProvider(getProvider(this._urlBase));
-  }
+	addProvider() {
+		this._openmct.telemetry.addProvider(getProvider(this._urlBase));
+	}
 }
 
 function getProvider(urlBase) {
-  return {
-    supportsRequest: function(domainObject) {
-      return domainObject.type === 'sat.telemetry';
-    },
-    request: function(domainObject, options) {
-      const name = parseNamespace(domainObject.identifier.namespace);
-      var url = `${urlBase}${name}.${domainObject.name}/${options.start}/${options.end}`;
+	return {
+		supportsRequest: function(domainObject) {
+			return domainObject.type === "sat.telemetry";
+		},
+		request: function(domainObject, options) {
+			const name = parseNamespace(domainObject.identifier.namespace);
+			var url = `${urlBase}${name}.${domainObject.name}/${options.start}/${options.end}`;
+			url = sanitizeURL(url);
 
-      return axios.get(url).then(function(resp) {
+			return axios.get(url).then(function(resp) {
 				if (resp.data.length === 0) {
-					// hay que cambiar este hardcodeo cuando este implementado
-					// en el backend.
-					url = `${urlBase}${name}.${domainObject.name}/1531958932000/1539734400000`;
-					return axios.get(url).then(newResp => {
-						let histData = newResp.data;
-						console.log(histData);
-						if (histData.length != 0) {
-							let first = histData[0].timestamp,
-								last = histData[histData.length - 1].timestamp;
-							openmct.time.bounds({start: first, end: last});
-							return histData;
+					url = `${urlBase}${name}.${domainObject.name}/`;
+
+					return axios.get(url).then(
+						newResp => {
+							let histData = newResp.data,
+								lastData = [];
+
+							if (histData.length != 0) {
+								let first = histData[0].timestamp,
+									last = histData[histData.length - 1].timestamp;
+								openmct.time.bounds({ start: first, end: last });
+								lastData = histData;
+							}
+
+							return lastData;
+						},
+						err => {
+							return [];
 						}
-								return [];
-					});
-				}
-        return resp.data;
-      });
-    }
-  };
+					);
+				} else return resp.data;
+			});
+		}
+	};
 }
 
 function parseNamespace(namespace) {
-  return namespace.replace('.telemetry', '');
+	return namespace.replace(".telemetry", "");
+}
+
+function sanitizeURL(url) {
+	return url.replace(/\/(\d*)\.\d*/g, "/$1");
 }
